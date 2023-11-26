21bf706ccff84979df4eee30006dc0bad6cf8442
Nicholas Lee <niconal902@gmail.com>
2023-11-26 11:08:14 -0500

    feat: added push notifications

--- grandpapp-frontend-v2/src/Listener/Listener.tsx
+++ grandpapp-frontend-v2/src/Listener/Listener.tsx
@@ -1,86 +1,108 @@
 import { useEffect, useRef, useState } from "react";
+import addNotification from "react-push-notification";
 
 const Listener: React.FC = (props) => {
-    const [messageHistory, setMessageHistory] = useState<{ user: string, color: string, message: string }[]>([])
-    const [connected, setConnected] = useState(false);
+  const [messageHistory, setMessageHistory] = useState<
+    { user: string; color: string; message: string }[]
+  >([]);
+  const [connected, setConnected] = useState(false);
 
-    const elementRef = useRef<HTMLDivElement>(null);
+  const elementRef = useRef<HTMLDivElement>(null);
 
-    useEffect(() => {
-        let eventSource: EventSource;
-        // PROD
-        eventSource = new EventSource('https://grandappv2.onrender.com/listen');
-        // DEV
-        // eventSource = new EventSource('http://localhost:3001/listen');
+  useEffect(() => {
+    let eventSource: EventSource;
+    // PROD
+    eventSource = new EventSource("https://grandappv2.onrender.com/listen");
+    // DEV
+    // eventSource = new EventSource('http://localhost:3001/listen');
 
-        eventSource.onopen = (event: any) => {
-            setConnected(true)
-        }
+    eventSource.onopen = (event: any) => {
+      setConnected(true);
+    };
 
-        eventSource.onmessage = (e: any) => {
-            console.log('hello');
-            
-            setMessageHistory((prevMessageHistory) => {
-                const receivedMessageObj = JSON.parse(e.data);                    
-                const newMessageHistory = [...prevMessageHistory];
-                newMessageHistory.push({
-                    user: receivedMessageObj.user,
-                    message: receivedMessageObj.transcript,
-                    color: receivedMessageObj.color
-                })
-                
-                return newMessageHistory;
-            })
-        }
+    eventSource.onmessage = (e: any) => {
+      setMessageHistory((prevMessageHistory) => {
+        const receivedMessageObj = JSON.parse(e.data);
+        const newMessageHistory = [...prevMessageHistory];
+        newMessageHistory.push({
+          user: receivedMessageObj.user,
+          message: receivedMessageObj.transcript,
+          color: receivedMessageObj.color,
+        });
 
-        eventSource.onerror = (e: any) => {
-            eventSource.close();
-            setConnected(false)
-        }
+        return newMessageHistory;
+      });
 
-        const cleanup = () => {
-            if (eventSource.OPEN) eventSource.close();
-        }
+      addNotification({
+        title: "warning",
+        native: true,
+      });
+    };
 
-        window.addEventListener('beforeunload', cleanup)
+    eventSource.onerror = (e: any) => {
+      eventSource.close();
+      setConnected(false);
+    };
 
-        return () => {
-            window.removeEventListener('beforeunload', cleanup)
-            if (eventSource.OPEN) eventSource.close();
-            setConnected(false)
-        }
-    }, [])
+    const cleanup = () => {
+      if (eventSource.OPEN) eventSource.close();
+    };
 
-    useEffect(() => {
-        if (elementRef?.current) {
-            const offsetBottom = elementRef.current.offsetTop + elementRef.current.offsetHeight;                
-            window.scrollTo({ top: offsetBottom, behavior: 'smooth' });
-        }
-    }, [messageHistory])
+    window.addEventListener("beforeunload", cleanup);
 
-    return (
-        <div ref={elementRef} style={{ padding: '2rem' }}>
-            <div style={{
-                fontSize: '1rem',
-                position: 'sticky',
-                backgroundColor: connected ? 'lightgreen' : '#ff9a93',
-                padding: '1rem',
-                right: 0,
-                top: 0,
-                textAlign: 'center',
-                color: connected ? 'green' : 'red'
-            }}>{ connected ? 'CONNECTED' : 'NOT CONNECTED' }</div>
-            {messageHistory.length === 0 && (
-                <div style={{ color: '#afaf00', fontSize: '6rem' }}>No messages</div>
-            )}
-            {messageHistory.map((messageObj, index) => (
-                <div key={index} style={{ backgroundColor: messageObj.color, color: 'white', borderRadius: '4px', padding: '1rem', marginBottom: '0.5rem' }}>
-                    <h1 style={{ margin: 0, fontSize: '4rem' }}><b>{messageObj.user}</b></h1>
-                    <div style={{ fontSize: '6rem' }}>{messageObj.message}</div>
-                </div>
-            ))}
+    return () => {
+      window.removeEventListener("beforeunload", cleanup);
+      if (eventSource.OPEN) eventSource.close();
+      setConnected(false);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (elementRef?.current) {
+      const offsetBottom =
+        elementRef.current.offsetTop + elementRef.current.offsetHeight;
+      window.scrollTo({ top: offsetBottom, behavior: "smooth" });
+    }
+  }, [messageHistory]);
+
+  return (
+    <div ref={elementRef} style={{ padding: "2rem" }}>
+      <div
+        style={{
+          fontSize: "1rem",
+          position: "sticky",
+          backgroundColor: connected ? "lightgreen" : "#ff9a93",
+          padding: "1rem",
+          right: 0,
+          top: 0,
+          textAlign: "center",
+          color: connected ? "green" : "red",
+        }}
+      >
+        {connected ? "CONNECTED" : "NOT CONNECTED"}
+      </div>
+      {messageHistory.length === 0 && (
+        <div style={{ color: "#afaf00", fontSize: "6rem" }}>No messages</div>
+      )}
+      {messageHistory.map((messageObj, index) => (
+        <div
+          key={index}
+          style={{
+            backgroundColor: messageObj.color,
+            color: "white",
+            borderRadius: "4px",
+            padding: "1rem",
+            marginBottom: "0.5rem",
+          }}
+        >
+          <h1 style={{ margin: 0, fontSize: "4rem" }}>
+            <b>{messageObj.user}</b>
+          </h1>
+          <div style={{ fontSize: "6rem" }}>{messageObj.message}</div>
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
 
 export default Listener;
--- grandpapp-frontend-v2/src/Sender/Sender.tsx
+++ grandpapp-frontend-v2/src/Sender/Sender.tsx
@@ -1,150 +1,190 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMicrophone, faStopCircle } from '@fortawesome/free-solid-svg-icons'
-import './Sender.css';
-import { useEffect, useState } from 'react';
-import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
-import axios from 'axios'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMicrophone, faStopCircle } from "@fortawesome/free-solid-svg-icons";
+import "./Sender.css";
+import { useEffect, useState } from "react";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+import axios from "axios";
 
 enum ELang {
-    'en-US' = 'en-US',
-    'zh-CN' = 'zh-CN',
-    'ja' = 'ja'
+  "en-US" = "en-US",
+  "zh-CN" = "zh-CN",
+  "ja" = "ja",
 }
 
 const Sender: React.FC = (props) => {
-    const [name, setName] = useState('');
-    const [color, setColor] = useState<string>('');
-    const [language, setLanguage] = useState<ELang>(ELang['en-US']);
-    const [isListening, setIsListening] = useState(false);
+  const [name, setName] = useState("");
+  const [color, setColor] = useState<string>("");
+  const [language, setLanguage] = useState<ELang>(ELang["en-US"]);
+  const [isListening, setIsListening] = useState(false);
 
-    const [submitted, setSubmitted] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
 
-    const { 
-        transcript,
-        resetTranscript,
-        browserSupportsSpeechRecognition,
-        isMicrophoneAvailable
-    } = useSpeechRecognition();
+  const {
+    transcript,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+    isMicrophoneAvailable,
+  } = useSpeechRecognition();
 
-    const disabled = color === undefined || color.length === 0 || name.length === 0 || language.length === 0;
+  const disabled =
+    color === undefined ||
+    color.length === 0 ||
+    name.length === 0 ||
+    language.length === 0;
 
-    const handleSubmit = () => {
-        if (name.length && name.length > 0 && color && color.length > 0) {
-            localStorage.setItem('grandpapp-name', name);
-            localStorage.setItem('grandpapp-color', color);
-            localStorage.setItem('grandpapp-language', language);
-            setSubmitted(true)
-        } 
+  const handleSubmit = () => {
+    if (name.length && name.length > 0 && color && color.length > 0) {
+      localStorage.setItem("grandpapp-name", name);
+      localStorage.setItem("grandpapp-color", color);
+      localStorage.setItem("grandpapp-language", language);
+      setSubmitted(true);
     }
+  };
 
-    const handleStartListen = () => {
-        resetTranscript();
-        SpeechRecognition.startListening({ continuous: true, language: language });
-        setIsListening(true);
+  const handleStartListen = () => {
+    resetTranscript();
+    SpeechRecognition.startListening({ continuous: true, language: language });
+    setIsListening(true);
+  };
+
+  const handleStopListen = async () => {
+    await SpeechRecognition.stopListening();
+    setIsListening(false);
+
+    if (transcript.length > 0) {
+      // PROD
+      axios.post("https://grandappv2.onrender.com/listen", {
+        user: name,
+        transcript: transcript,
+        color: color,
+      });
+      // DEV
+      // axios.post('http://localhost:3001/listen', { user: name, transcript: transcript, color: color })
     }
+  };
 
-    const handleStopListen = async () => {
-        await SpeechRecognition.stopListening();
-        setIsListening(false)
+  const handleCancelListen = async () => {
+    await SpeechRecognition.abortListening();
+    resetTranscript();
+    setIsListening(false);
+  };
 
-        
-        if (transcript.length > 0) {
-            // PROD
-            axios.post('https://grandappv2.onrender.com/listen', { user: name, transcript: transcript, color: color })
-            // DEV
-            // axios.post('http://localhost:3001/listen', { user: name, transcript: transcript, color: color })
-        }
-    }
+  useEffect(() => {
+    const name = localStorage.getItem("grandpapp-name");
+    const color = localStorage.getItem("grandpapp-color");
+    const language = localStorage.getItem("grandpapp-language") as ELang;
 
-    const handleCancelListen = async () => {
-        await SpeechRecognition.abortListening();
-        resetTranscript()
-        setIsListening(false);
-    }
+    setName(name || "");
+    setColor(color || "");
+    setLanguage(language || ELang["en-US"]);
+  }, []);
 
-    useEffect(() => {
-        const name = localStorage.getItem('grandpapp-name');
-        const color = localStorage.getItem('grandpapp-color');
-        const language = localStorage.getItem('grandpapp-language') as ELang;
+  return (
+    <div
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      {browserSupportsSpeechRecognition || isMicrophoneAvailable ? (
+        submitted ? (
+          <>
+            <p>Click to record and send your voice</p>
+            {isListening ? (
+              <FontAwesomeIcon
+                onClick={handleStopListen}
+                className="mic"
+                style={{ color: isListening ? "green" : "gray" }}
+                icon={faStopCircle}
+              />
+            ) : (
+              <FontAwesomeIcon
+                onClick={handleStartListen}
+                className="mic"
+                style={{ color: isListening ? "green" : "gray" }}
+                icon={faMicrophone}
+              />
+            )}
+            {isListening && (
+              <button
+                onClick={handleCancelListen}
+                style={{ marginTop: "1rem", maxWidth: "300px" }}
+                className="button no"
+              >
+                CANCEL
+              </button>
+            )}
+            <div style={{ padding: "1rem" }}>
+              <p style={{ fontSize: "2rem" }}>{transcript}</p>
+            </div>
+          </>
+        ) : (
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              padding: "1rem",
+            }}
+          >
+            <h1>What is your name?</h1>
+            <input
+              placeholder="name"
+              style={{
+                padding: "1rem",
+                marginBottom: "1rem",
+              }}
+              value={name}
+              onChange={(event) => {
+                setName(event.target.value);
+              }}
+            />
+            <select
+              value={color}
+              onChange={(event) => {
+                setColor(event.target.value);
+              }}
+              className="select"
+              style={{ marginBottom: "1rem" }}
+            >
+              <option value="">select color</option>
+              <option value="green">green</option>
+              <option value="blue">blue</option>
+              <option value="orange">orange</option>
+              <option value="red">red</option>
+              <option value="purple">purple</option>
+              <option value="#dbdb2c">yellow</option>
+              <option value="darkgray">gray</option>
+              <option value="brown">brown</option>
+            </select>
+            <select
+              value={language}
+              onChange={(event) => {
+                setLanguage(event.target.value as ELang);
+              }}
+              className="select"
+            >
+              <option value="">select language</option>
+              <option value={ELang["en-US"]}>English</option>
+              <option value={ELang.ja}>Japanese</option>
+              <option value={ELang["zh-CN"]}>Chinese</option>
+            </select>
 
-        setName(name || '');
-        setColor(color || '');
-        setLanguage(language || ELang['en-US'])        
-    }, [])
-
-    return (
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-            {
-                (browserSupportsSpeechRecognition || isMicrophoneAvailable) ? (
-                    submitted ? (
-                        <>
-                            <p>Click to record and send your voice</p>
-                            {
-                                isListening ? (
-                                    <FontAwesomeIcon onClick={handleStopListen} className='mic' style={{ color: isListening ? 'green' : 'gray' }} icon={faStopCircle} />
-                                    ) : (
-                                    <FontAwesomeIcon onClick={handleStartListen} className='mic' style={{ color: isListening ? 'green' : 'gray' }} icon={faMicrophone} />
-                                )
-                            }
-                            {isListening && (
-                                <button onClick={handleCancelListen} style={{ marginTop: '1rem', maxWidth: '300px' }} className='button no'>CANCEL</button>
-                            )}
-                            <div style={{ padding: '1rem' }}>
-                                <p style={{ fontSize: '2rem' }}>{transcript}</p>
-                            </div>
-                        </>
-                    ) : (
-                        <div style={{ display: 'flex', flexDirection: 'column', padding: '1rem' }}>
-                            <h1>What is your name?</h1>
-                            <input
-                                placeholder='name'
-                                style={{
-                                    padding: '1rem',
-                                    marginBottom: '1rem',
-                                }}
-                                value={name}
-                                onChange={(event) => {
-                                    setName(event.target.value);
-                                }} />
-                            <select
-                                value={color}
-                                onChange={(event) => {
-                                    setColor(event.target.value)
-                                }}
-                                className="select"
-                                style={{ marginBottom: '1rem' }}>
-                                <option value="">select color</option>
-                                <option value="green">green</option>
-                                <option value="blue">blue</option>
-                                <option value="orange">orange</option>
-                                <option value="red">red</option>
-                                <option value="purple">purple</option>
-                                <option value="#dbdb2c">yellow</option>
-                                <option value="darkgray">gray</option>
-                                <option value="brown">brown</option>
-                            </select>
-                            <select
-                                value={language}
-                                onChange={(event) => {
-                                    setLanguage(event.target.value as ELang)
-                                }}
-                                className="select"
-                            >
-                                <option value="">select language</option>
-                                <option value={ELang['en-US']}>English</option>
-                                <option value={ELang.ja}>Japanese</option>
-                                <option value={ELang['zh-CN']}>Chinese</option>
-                            </select>
-            
-                            <button onClick={handleSubmit} className={`yes-button ${disabled ? 'disabled' : ''}`} style={{ marginTop: '1rem', cursor: 'pointer' }}>Submit</button>
-                        </div>
-                    )
-                ) : (
-                    <span>This browser does not support speech recognition, or the mic is not available</span>
-                )
-            }
-        </div>
-    )
-}
+            <button
+              onClick={handleSubmit}
+              className={`yes-button ${disabled ? "disabled" : ""}`}
+              style={{ marginTop: "1rem", cursor: "pointer" }}
+            >
+              Submit
+            </button>
+          </div>
+        )
+      ) : (
+        <span>
+          This browser does not support speech recognition, or the mic is not
+          available
+        </span>
+      )}
+    </div>
+  );
+};
 
 export default Sender;
